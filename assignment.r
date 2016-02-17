a06ea2278a361415172b87429955e5bd09578a65
fionc <fionc@uw.edu>
2016-02-17 14:05:35 -0800

    added some code

---
+++ assignment.r
@@ -0,0 +1,9 @@
+# some code
+# Create a vector "x" that has the values 10 through 20 in it (using the : operator)
+x <- 10:20
+
+# Create a vector "y" that has the values 21 through 30 in it (using the seq operator)
+y<- seq(21,30)
+
+# Create a vector "z" by combining the vectors "x" and "y"
+z <- c(x, y)
